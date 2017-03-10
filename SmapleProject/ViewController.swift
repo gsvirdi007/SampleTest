f8a7c4e002b3fcd47edbc7730b69df766d8866c7
Softdata infotech <gurpreet@Softdatas-Mac-mini.local>
2017-03-10 13:02:18 +0530

    testing 123

--- SmapleProject/ViewController.swift
+++ SmapleProject/ViewController.swift
@@ -18,7 +18,16 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        //ewrwerwerwerewrerwer
+        //ewrwerwerwerewrerwer//ewrwerwerwerewrerwer//ewrwerwerwerewrerwer
+        //ewrwerwerwerewrerwer//ewrwerwerwerewrerwer//ewrwerwerwerewrerwer
+
+        //ewrwerwerwerewrerwer//ewrwerwerwerewrerwer//ewrwerwerwerewrerwer
+
+        //ewrwerwerwerewrerwer//ewrwerwerwerewrerwer//ewrwerwerwerewrerwer
+        //ewrwerwerwerewrerwer//ewrwerwerwerewrerwer//ewrwerwerwerewrerwer
+        //ewrwerwerwerewrerwer//ewrwerwerwerewrerwer//ewrwerwerwerewrerwer
+
+        
     }
 
 
